c886eb1e13d0d852a111f13668d6520f2fd9389d
Miguel Cardoso <miguel.antunes.cardoso>
2023-08-18 21:19:30 +0100

    Novos butoes e erro corrigido

--- lib/futuro.dart
+++ lib/futuro.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -20,75 +21,141 @@ class _FirstPageState extends State<FirstPage> {
 
 String final_response = "aaaa";
 final picker = ImagePicker();
-int check_image = 0;             // verifica se alguma imagem foi selcionada (Sim == 1; Nao == 0)
+int check_image = 1;             // verifica se alguma imagem foi selcionada (Sim == 1; Nao == 0)
 //File? file;                   //variavel do tipo ficheiro que guarda a imagem que sai da funçao, caso nao web
 Uint8List web_image = Uint8List(8); // guarda a imagem que sai da funçao caso web
-
+Uint8List b = Uint8List(8);
+final isDialOpen = ValueNotifier(false);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        backgroundColor: Colors.grey[300],
-      ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 100,
-            width: 500,
-            child: check_image == 0?
-            Container(
-              child: Text(
-                'Press the button to choose a photo and start counting your bugs :)'
-              )
-            ):
+    return WillPopScope(                  //isto serve para quando se carregar na seta de voltar atras com o menu aberto, sairmos do menu
+                                          // em vez de sairmos da aplicaçao
+      onWillPop: ()async{
+        if(isDialOpen.value){
+
+          isDialOpen.value = false;
+          return false;
+        }
+        else{
+          return true;
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Home"),
+          backgroundColor: Colors.grey[300],
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              height: 100,
+              width: 500,
+              child: listEquals(web_image, b)?
+              Container(
+                padding: EdgeInsets.only(top: 30),
+                child: Text(
+                  'Press the button to choose a photo and start counting your bugs :)'
+                )
+              ):
+              
+                Image.memory(web_image, fit: BoxFit.cover),
+            ),
             
-              Image.memory(web_image, fit: BoxFit.cover),
-          ),
-          
-          FloatingActionButton(
-            onPressed: () async{
-                var a = await F().getImage(true, web_image);           //A camera n esta a funcionar em web, mas funciona com o Android
-
-                setState(() {
-                  web_image = a;
-                  check_image = 1;
-                });
-            }, 
-
-            child: Icon(Icons.camera),
-          ),
-          FloatingActionButton(
-            onPressed: ()async{
-                var a = await F().getImage(false, web_image);
-          
-                setState(() {
-                  web_image = a;
-                  check_image = 1;
-                });
-            }, 
-            child: Icon(Icons.file_download),
-          ),
-          
-
-
-          FloatingActionButton(
-            child: Icon(Icons.send),
-
-            onPressed: ()async{
-             var responde = await BaseClient().post(web_image); //passa-se a string ola para o api.py
-             var teste = await BaseClient().get(); //recebe-se a string modificada
-             
-              setState(() {
-                web_image = teste; // passa-se a string modificada para final_response
-              });
-            },
-          ),
-          //Text(final_response), // mostra-se a string modificada
-
-        ],
-      )
+            Container(
+              padding: EdgeInsets.only(bottom: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                 /* FloatingActionButton(
+                    onPressed: () async{
+                        var a = await F().getImage(true, web_image);           //A camera n esta a funcionar em web, mas funciona com o Android
+    
+                        setState(() {
+                          web_image = a;
+                          check_image = 1;
+                        });
+                    }, 
+    
+                    child: Icon(Icons.camera),
+                  ),
+                  FloatingActionButton(
+                    onPressed: ()async{
+                        var a = await F().getImage(false, web_image);
+                  
+                        setState(() {
+                          web_image = a;
+                          check_image = 1;
+                        });
+                    }, 
+                    child: Icon(Icons.file_download),
+                  ), */
+                  
+    
+    
+                  FloatingActionButton(
+                    child: Icon(Icons.send),
+    
+                    onPressed: ()async{
+                     var responde = await BaseClient().post(web_image); //passa-se a string ola para o api.py
+                     var teste = await BaseClient().get(); //recebe-se a string modificada
+                     
+                      setState(() {
+                        web_image = teste; // passa-se a string modificada para final_response
+                      });
+                    },
+                  ),
+    
+                  SpeedDial(
+                    animatedIcon: AnimatedIcons.menu_close, //por os botoes bonitos
+                    backgroundColor: Colors.black,
+                    overlayColor: Colors.black,
+                    overlayOpacity: 0.4,
+                    spacing: 10,
+                    spaceBetweenChildren: 12,
+                    openCloseDial: isDialOpen,    //isto serve para quando se carregar na seta de voltar atras com o menu aberto, sairmos do menu
+                                                  // em vez de sairmos da aplicaçao
+                    children: [
+                      SpeedDialChild(
+                        child: Icon(Icons.file_download),
+                        label: 'Galeria',
+                        onTap: ()async{
+                          var a = await F().getImage(false, web_image);
+                  
+                        setState(() {
+                          web_image = a;
+                          check_image = 1;
+                        });
+                        }
+                      ),
+    
+                      SpeedDialChild(
+                        child: Icon(Icons.camera),
+                        label: 'Camera',
+                        onTap: ()async{
+                          
+                        var a = await F().getImage(true, web_image);           //A camera n esta a funcionar em web, mas funciona com o Android
+                          
+                        setState(() {
+  
+                          print(web_image);
+                          web_image = a;
+                          check_image = 1;
+                          
+                        });
+                        },
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            //Text(final_response), // mostra-se a string modificada
+    
+          ],
+        )
+      ),
     );
   }
 }
